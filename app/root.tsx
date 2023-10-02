8cd254d9dbf1044bdd0dc98244b2ab6b0cf902f6
duoluodexiaoxiaoyuan <1203125214@qq.com>
2023-10-02 09:39:21 +0800

    feat:代码提交

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
-import { json, redirect } from '@remix-run/node';
-import { useEffect, useState } from 'react';
+import { useChangeLanguage } from "remix-i18next";
+import { json } from "@remix-run/node";
+import { useTranslation } from "react-i18next";
+import i18next from "~/i18next.server";
 import {
   Form,
   Link,
@@ -15,129 +17,40 @@ import {
   useSubmit,
 } from '@remix-run/react';
 
-import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
-// existing imports
-import { createEmptyContact, getContacts } from './data';
+export async function loader({ request }: any) {
+  let locale = await i18next.getLocale(request);
+  return json({ locale });
+}
 
-export const action = async () => {
-  const contact = await createEmptyContact();
-  // return json({ contact });
-  return redirect(`/contacts/${contact.id}/edit`);
+export let handle = {
+  // In the handle export, we can add a i18n key with namespaces our route
+  // will need to load. This key can be a single string or an array of strings.
+  // TIP: In most cases, you should set this to your defaultNS from your i18n config
+  // or if you did not set one, set it to the i18next default namespace "translation"
+  i18n: "common",
 };
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  const url = new URL(request.url);
-  const q = url.searchParams.get('q');
-  const contacts = await getContacts(q);
-  return json({ contacts, q });
-};
+export default function Root() {
+  // Get the locale from the loader
+  let { locale } = useLoaderData<typeof loader>();
 
-import appStylesHref from './app.css';
+  let { i18n } = useTranslation();
 
-export const links: LinksFunction = () => [
-  { rel: 'stylesheet', href: appStylesHref },
-];
+  // This hook will change the i18n instance language to the current locale
+  // detected by the loader, this way, when we do something to change the
+  // language, this locale will change and i18next will load the correct
+  // translation files
+  useChangeLanguage(locale);
 
-export default function App() {
-  // const { contacts } = useLoaderData();
-  const { contacts, q } = useLoaderData<typeof loader>();
-  const navigation = useNavigation();
-  // the query now needs to be kept in state
-  const [query, setQuery] = useState(q || '');
-  const submit = useSubmit();
-  const searching =
-    navigation.location &&
-    new URLSearchParams(navigation.location.search).has(
-      "q"
-    );
-
-  // useEffect(() => {
-  //   const searchField = document.getElementById("q");
-  //   if (searchField instanceof HTMLInputElement) {
-  //     searchField.value = q || "";
-  //   }
-  // }, [q]);
-
-  // we still have a `useEffect` to synchronize the query
-  // to the component state on back/forward button clicks
-  useEffect(() => {
-    setQuery(query);
-  }, [query]);
   return (
-    <html lang="en">
+    <html lang={locale} dir={i18n.dir()}>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form
-              id="search-form"
-              role="search"
-              onChange={(event) => {
-                const isFirstSearch = q === null;
-                submit(event.currentTarget, {
-                  replace: !isFirstSearch,
-                });
-              }}
-            >
-              <input
-                id="q"
-                defaultValue={q || ''}
-                aria-label="Search contacts"
-                className={searching ? "loading" : ""}
-                placeholder="Search"
-                type="search"
-                name="q"
-                // synchronize user's input to component state
-                onChange={(event) => setQuery(event.currentTarget.value)}
-                // switched to `value` from `defaultValue`
-                value={query}
-              />
-              <div id="search-spinner" aria-hidden hidden={!searching}/>
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <NavLink to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{' '}
-                      {contact.favorite ? <span>★</span> : null}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
-        <div
-          id="detail"
-          className={navigation.state === "loading" && !searching
-          ? "loading"
-          : ""}
-        >
-          <Outlet />
-        </div>
-
+        <div>你好世界</div>
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,19 @@
+import { useTranslation } from "react-i18next";
 export default function Index() {
+  let { t, i18n} = useTranslation();
   return (
-    <p id="index-page">
+    <div>
+      <p id="index-page">
       This is a demo for Remix.
       <br />
       Check out{" "}
       <a href="https://remix.run">the docs at remix.run</a>.
     </p>
+    <h1>{t("greeting", {lng: 'es'})}</h1>
+    {/* {Object.keys(lngs)} */}
+    <button onClick={() => {
+      i18n.changeLanguage("es")
+    }}>切换</button>
+    </div>
   );
 }
